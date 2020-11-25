74f28e21e33b073eded7318640ed52d2f360bcff
John Wass <jwass3@gmail.com>
2020-11-25 15:05:22 -0500

    initial import

---
+++ conanfile.py
@@ -0,0 +1,22 @@
+from conans import ConanFile
+
+
+class ParticlePackage(ConanFile):
+    name = 'OneWire'
+    version = 'c5007f4'
+    url = 'https://github.com/hicktech/conan-OneWire'
+    repo_url = 'https://github.com/particle-iot/OneWireLibrary.git'
+    generators = 'cmake'
+    settings = []
+    requires = []
+
+    def package(self):
+        self.copy('*.c*', dst='src')
+        self.copy('*.h*', dst='include')
+
+    def source(self):
+        self.run(f'git clone {self.repo_url} .')
+        self.run(f'git checkout {self.version}')
+
+    def package_info(self):
+        self.cpp_info.srcdirs = ['src']
